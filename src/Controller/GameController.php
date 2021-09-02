a2cb35291c56262bf3e5fe9e02a6be23a37761d0
Edén <claudio.ruiz@bootcamp-upgrade.com>
2021-09-02 14:16:13 +0200

    FEAT: created game controller, added TODOs to user controller

---
+++ src/Controller/GameController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Game;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class GameController extends AbstractController
+{
+    /**
+     * @Route("/games", name="gameGallery")
+     */
+    public function listGames(EntityManagerInterface $doctrine)
+    {
+        $repo = $doctrine->getRepository(Game::class);
+        $games = $repo->findAll();
+
+        return $this->render("game/game-gallery.html.twig", ["games" => $games]);
+    }
+
+    /**
+     * @Route("/game/{id}", name="gameView")
+     */
+    public function showGame($id, EntityManagerInterface $doctrine)
+    {
+        $repo = $doctrine->getRepository(Game::class);
+        $game = $repo->find($id);
+
+        return $this-> render("game/game-view.html.twig", ["game" => $game]);
+    }
+}
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 class UserController extends AbstractController
 {
@@ -18,40 +19,70 @@ class UserController extends AbstractController
     /**
      * @Route("/user/my-games", name="myGames")
      */
-    public function myGames()
+    public function myGames(EntityManagerInterface $doctrine)
     {
-        return $this->render("/user/my-games.html.twig");
+        // TODO: Obtener id de usuario loggeado y usarlo para encontrar todos los juegos con dicho id como "owner".
+        $repo = $doctrine->getRepository(Game::class);
+        // $games = $repo->findBy(["owner"=>$userId]);
+
+        // return $this->render("/user/my-games.html.twig", ["games" => $games]);
     }
 
-    /**
-     * @Route("/user/my-exchanges", name="myExchanges")
-     */
-    public function myExchanges()
-    {
-        return $this->render("/user/my-exchanges.html.twig");
-    }
-    
     /**
      * @Route("/user/game/new", name="addGame")
      */
     public function addGame()
     {
+        // TODO: Añadir formulario de creación.
         return $this->render("/user/add-game.html.twig");
     }
-    
+
     /**
      * @Route("/user/game/edit/{id}", name="editGame")
      */
     public function editGame()
     {
+        // TODO: Añadir formulario de edición.
         return $this->render("/user/edit-game.html.twig");
     }
-        
+
+    /**
+     * @Route("/user/game/delete/{id}", name="deleteGame")
+     */
+    public function deleteGame($id, EntityManagerInterface $doctrine)
+    {
+        $repo = $doctrine->getRepository(Game::class);
+        $game = $repo->find($id);
+
+        $doctrine->remove($game);
+        $doctrine->flush();
+
+        return $this->redirectToRoute("listPokemons");
+        return $this->redirectToRoute("/user/my-games.html.twig");
+    }
+
+    /**
+     * @Route("/user/my-exchanges", name="myExchanges")
+     */
+    public function myExchanges(EntityManagerInterface $doctrine)
+    {
+        // TODO: Obtener operations del usuario loggeado y pintar un solo div.
+        $repo = $doctrine->getRepository(Operation::class);
+        // $exchanges = $repo->findBy(["borrower"=>$userId, "lender"=>$userId]);
+
+        // return $this->render("/user/my-exchanges.html.twig", ["exchanges" => $exchanges]);
+    }
+
     /**
      * @Route("/user/exchange/{id}", name="exchangeView")
      */
-    public function exchangeView()
+    public function exchangeView($id, EntityManagerInterface $doctrine)
     {
-        return $this->render("/user/exchange-view.html.twig");
+        // TODO: Obtener las operations con el operationId y usarlas para mostrar diferente información sobre el intercambio.
+        $repo = $doctrine->getRepository(Operation::class);
+        $operationsAsBorrower = $repo->find($id);
+        $operationsAsLender = $repo->find($id);
+
+        return $this->render("/user/exchange-view.html.twig", ["operationsAsBorrower" => $operationsAsBorrower, "operationsAsLender" => $operationsAsLender]);
     }
 }
